5a61dd7fc9981e5d679c3f532384cb8ca5cbae56
limengzhe <limzv@live.cn>
2022-10-06 00:04:35 +0800

    initial

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,9 +89,9 @@ export default {
       })
     },
   },
-  generate: {
-    minify: {
-      collapseWhitespace: false,
-    },
-  },
+  // generate: {
+  //   minify: {
+  //     collapseWhitespace: false,
+  //   },
+  // },
 }
